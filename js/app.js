7c7705b27d0de32ccb0ca686bb2facf1ab131205
hilmanalfarabi <hilmanalfarabi@gmail.com>
2020-06-07 21:39:33 +0700

    ubah carousel, icon dan asset gambar

--- js/app.js
+++ js/app.js
@@ -4,108 +4,128 @@
 */
 
 
-! function($) {
+! function ($) {
     "use strict";
 
-    var Invoza = function() {};
+    var Invoza = function () {};
 
-    Invoza.prototype.initStickyMenu = function() {
-        $(window).scroll(function() {
-            var scroll = $(window).scrollTop();
-        
-            if (scroll >= 50) {
-                $(".sticky").addClass("nav-sticky");
-            } else {
-                $(".sticky").removeClass("nav-sticky");
-            }
-        });
-    },
+    Invoza.prototype.initStickyMenu = function () {
+            $(window).scroll(function () {
+                var scroll = $(window).scrollTop();
 
-    Invoza.prototype.initSmoothLink = function() {
-        $('.navbar-nav a').on('click', function(event) {
-            var $anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $($anchor.attr('href')).offset().top - 0
-            }, 1500, 'easeInOutExpo');
-            event.preventDefault();
-        });
-    },
+                if (scroll >= 50) {
+                    $(".sticky").addClass("nav-sticky");
+                } else {
+                    $(".sticky").removeClass("nav-sticky");
+                }
+            });
+        },
 
-    Invoza.prototype.initScrollspy = function() {
-        $("#navbarCollapse").scrollspy({
-            offset:20
-        });
-    },
+        Invoza.prototype.initSmoothLink = function () {
+            $('.navbar-nav a').on('click', function (event) {
+                var $anchor = $(this);
+                $('html, body').stop().animate({
+                    scrollTop: $($anchor.attr('href')).offset().top - 0
+                }, 1500, 'easeInOutExpo');
+                event.preventDefault();
+            });
+        },
 
-    Invoza.prototype.initTesticarousel = function() {
-        $('#testi-carousel').owlCarousel({
-            items: 1,
-            loop:true,
-            margin:10,
-            nav:true,
-            responsive:{
-                576:{
-                    items:2
-                },
-     
-            }
-        });
-    },
+        Invoza.prototype.initScrollspy = function () {
+            $("#navbarCollapse").scrollspy({
+                offset: 20
+            });
+        },
 
-    
+        Invoza.prototype.initTesticarousel = function () {
+            $('#testi-carousel').owlCarousel({
+                items: 1,
+                loop: true,
+                margin: 10,
+                nav: true,
+                responsive: {
+                    576: {
+                        items: 2
+                    },
 
-    Invoza.prototype.initCounter = function() {
-        // Counter Number
-        var a = 0;
-        $(window).scroll(function() {
-            var oTop = $('#counter').offset().top - window.innerHeight;
-            if (a == 0 && $(window).scrollTop() > oTop) {
-                $('.counter-value').each(function() {
-                    var $this = $(this),
-                        countTo = $this.attr('data-count');
-                    $({
-                        countNum: $this.text()
-                    }).animate({
-                            countNum: countTo
-                        },
+                }
+            });
+        },
 
-                        {
+        Invoza.prototype.initLayanancarousel = function () {
+            $('#layanan-carousel').owlCarousel({
+                items: 1,
+                loop: true,
+                margin: 10,
+                nav: false,
+                responsive: {
+                    0: {
+                        items: 1
+                    },
+                    600: {
+                        items: 2
+                    },
+                    1000: {
+                        items: 3
+                    }
 
-                            duration: 2000,
-                            easing: 'swing',
-                            step: function() {
-                                $this.text(Math.floor(this.countNum));
+                }
+            });
+        },
+
+        Invoza.prototype.initCounter = function () {
+            // Counter Number
+            var a = 0;
+            $(window).scroll(function () {
+                var oTop = $('#counter').offset().top - window.innerHeight;
+                if (a == 0 && $(window).scrollTop() > oTop) {
+                    $('.counter-value').each(function () {
+                        var $this = $(this),
+                            countTo = $this.attr('data-count');
+                        $({
+                            countNum: $this.text()
+                        }).animate({
+                                countNum: countTo
                             },
-                            complete: function() {
-                                $this.text(this.countNum);
-                                //alert('finished');
-                            }
 
-                        });
-                });
-                a = 1;
-            }
-        });
+                            {
 
-    },
+                                duration: 2000,
+                                easing: 'swing',
+                                step: function () {
+                                    $this.text(Math.floor(this.countNum));
+                                },
+                                complete: function () {
+                                    $this.text(this.countNum);
+                                    //alert('finished');
+                                }
+
+                            });
+                    });
+                    a = 1;
+                }
+            });
+
+        },
 
 
-    feather.replace()
+        feather.replace()
 
-    Invoza.prototype.init = function() {
-        this.initStickyMenu();
-        this.initSmoothLink();
-        this.initScrollspy();
-        this.initTesticarousel();
-        this.initCounter();
-    },
-    //init
-    $.Invoza = new Invoza, $.Invoza.Constructor = Invoza
+    Invoza.prototype.init = function () {
+            this.initStickyMenu();
+            this.initSmoothLink();
+            this.initScrollspy();
+            this.initTesticarousel();
+            this.initLayanancarousel();
+            this.initCounter();
+        },
+        //init
+        $.Invoza = new Invoza, $.Invoza.Constructor = Invoza
 }(window.jQuery),
 
 
 //initializing
-function($) {
+function ($) {
     "use strict";
     $.Invoza.init();
 }(window.jQuery);
